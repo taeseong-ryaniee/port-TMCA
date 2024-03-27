066e03e2ef56d65687ba2ac2cf2430540d9c8e37
TaeSeong Ryan YI <taeseong.culture@gmail.com>
2024-03-27 17:23:15 +0900

    2456

---
+++ js/webfont.js
@@ -0,0 +1,11 @@
+WebFontConfig = {
+    typekit: { id: "eup8kwr" },
+};
+
+(function (d) {
+    var wf = d.createElement("script"),
+        s = d.scripts[0];
+    wf.src = "https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js";
+    wf.async = true;
+    s.parentNode.insertBefore(wf, s);
+})(document);
